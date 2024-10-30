07ca2e9d8285d4860da1da893a8e2b73e72c9712
fernandabonfat <fernandabonfat@hotmail.com>
2024-10-29 21:10:59 -0300

    first commit

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,32 @@
+
+// 1 - Bibliotecas
+
+import org.junit.jupiter.api.Test;
+
+// 2 - Classe
+public class TestUser {
+
+    // 2.1 - Atributos
+    static String ct = "application/json"; //Content type
+    static String uriUser = "petstore.swagger.io/v2/user"; 
+
+    // 2.2 - Funções e Métodos
+    // 2.2.1 - Funções e Métodos comuns /  úteis
+
+    // Função de leitura de json
+    public static  String lerArquivoJson(String arquivoJson) {
+        //TODO: "Completar"
+        //return new String()
+        return null;
+    }
+
+    // 2.2.2 Métodos de teste
+    @Test
+    public void testPostPet() {
+        // Carregar os dados do arquivo json pet
+       // String jsonBody = new 
+
+
+    }
+    
+}
